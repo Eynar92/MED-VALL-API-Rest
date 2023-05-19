6f86f4e2893cc7e135d7275e7d35171e5eaf770b
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-05-19 01:19:47 -0400

    We implement pagination

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,8 +31,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListadoMedico> listadoMedicos() {
-        return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 
 }
